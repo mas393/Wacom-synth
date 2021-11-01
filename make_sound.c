ddbb14fee6efdeb839e8b5d7e9ab08b7255afd4e
Matthew <matthewstephenson39@gmail.com>
2021-11-01 16:51:35 -0700

    simplified to use single large buffer for a sound

--- make_sound.c
+++ make_sound.c
@@ -1,11 +1,8 @@
- 
+#include <time.h> 
 #include <stdio.h>
-//#include <stdlib.h>
-//#include <string.h>
-//#include <sched.h>
+#include <stdlib.h>
 #include <getopt.h>
 #include <alsa/asoundlib.h>
-//#include <sys/time.h>
 #include <math.h>
  
 static char *device = "default";         /* playback device */
@@ -22,60 +19,94 @@ static int period_event = 0;                /* produce poll event after each per
 static snd_pcm_sframes_t buffer_size;
 static snd_pcm_sframes_t period_size;
 static snd_output_t *output = NULL;
- 
+
+static int sample_sec = 1;
+static int sample_size = 44100 * 2;
+
+static void populate_buff(signed short *samples, double freq, int partials)
+{
+  int res;
+  double loc;
+  unsigned char *ptr = (unsigned char *)samples;
+  unsigned int maxval = (1 << 15) - 1;
+  for (int i = 0; i < sample_size/2; i++)
+    {
+      loc = (double)i/sample_size*2;
+      //      printf("%f\n", loc);
+      double a = 2*M_PI*freq*loc;
+
+      res = sin(a)*maxval;
+
+      // partials
+      for (int j = 1; j < partials; j++)
+	{
+	  a = a*2;
+	  res += sin(a)*maxval/pow(2, j)/pow(2, j);
+	}
+
+      // sound shape
+      res *= sin(loc*M_PI)/2;
+
+
+
+      for (int j = 0; j < 2; j++)
+	{
+	  //something weird if ptr is signed short as opposed to unsigned char
+	  *(ptr + j) = (res >> j * 8) & 0xff;	  
+	}
+
+      ptr = ptr + 2;
+
+    }
+  //  printf("loc %f", loc);
+}
+
 static void generate_sine(const snd_pcm_channel_area_t *areas, 
 			  snd_pcm_uframes_t offset,
-			  int count, double *_phase, double freq)
+			  int count, double *_phase, double freq, int partials)
 {
     static double max_phase = 2. * M_PI;
     double phase = *_phase;
     double step = max_phase*freq/(double)rate;
-    unsigned char *samples[channels];
-    int steps[channels];
-    unsigned int chn;
-    int format_bits = snd_pcm_format_width(format);
+    //    printf("rate = %d step = %f\n", rate, step); //rate = 44100 step = 0.060346
+    unsigned char *samples;
+    int steps;
+    int format_bits = snd_pcm_format_width(format); // = 16
     unsigned int maxval = (1 << (format_bits - 1)) - 1;
+    //    printf("maxval = %d\n", maxval); //maxval = 32767
     int bps = format_bits / 8;  /* bytes per sample */
-    int phys_bps = snd_pcm_format_physical_width(format) / 8;
-    int big_endian = snd_pcm_format_big_endian(format) == 1;
-    int to_unsigned = snd_pcm_format_unsigned(format) == 1;
-    int is_float = (format == SND_PCM_FORMAT_FLOAT_LE ||
-            format == SND_PCM_FORMAT_FLOAT_BE);
+    //    printf("bytes = %d\n",bps ); //bytes = 2
  
     /* verify and prepare the contents of areas */
-    for (chn = 0; chn < channels; chn++) {
-        if ((areas[chn].first % 8) != 0) {
-            printf("areas[%u].first == %u, aborting...\n", chn, areas[chn].first);
-            exit(EXIT_FAILURE);
-        }
-        samples[chn] = /*(signed short *)*/(((unsigned char *)areas[chn].addr) + (areas[chn].first / 8));
-        if ((areas[chn].step % 16) != 0) {
-            printf("areas[%u].step == %u, aborting...\n", chn, areas[chn].step);
-            exit(EXIT_FAILURE);
-        }
-        steps[chn] = areas[chn].step / 8;
-        samples[chn] += offset * steps[chn];
-    }
-    /* fill the channel areas */
+    samples = ((unsigned char *)areas[0].addr);
+    steps = areas[0].step / 8; // steps=2
+    
     while (count-- > 0) {
 
-        int res, i;
-
-	res = sin(phase) * maxval;
-
-        for (chn = 0; chn < channels; chn++) {
-            /* Generate data in native endian format */
-
+        int res, i, totaldecay;
+      
+	
+	res = sin(phase)*maxval;
+	totaldecay = 0;
+	for (int j = 1; j < partials; j++)
+	  {
+	    double p = phase * pow(2, j);
+	    res += sin(p)/pow(2,j)/pow(2,j)*maxval;
+	  }
+	res = res/2;
+	
 	  // we are little endian
-	  for (i = 0; i < bps; i++)
-	    *(samples[chn] + i) = (res >>  i * 8) & 0xff;
+	for (i = 0; i < bps; i++)
+	  *(samples + i) = (res >>  i * 8) & 0xff;
 
-	  samples[chn] += steps[chn];
-        }
+	samples += steps;
+        
         phase += step;
         if (phase >= max_phase)
             phase -= max_phase;
+	
     }
+
     *_phase = phase;
 }
  
@@ -89,75 +120,38 @@ static int set_hwparams(snd_pcm_t *handle,
  
     /* choose all parameters */
     err = snd_pcm_hw_params_any(handle, params);
-    if (err < 0) {
-        printf("Broken configuration for playback: no configurations available: %s\n", snd_strerror(err));
-        return err;
-    }
+
     /* set hardware resampling */
     err = snd_pcm_hw_params_set_rate_resample(handle, params, resample);
-    if (err < 0) {
-        printf("Resampling setup failed for playback: %s\n", snd_strerror(err));
-        return err;
-    }
+
     /* set the interleaved read/write format */
     err = snd_pcm_hw_params_set_access(handle, params, access);
-    if (err < 0) {
-        printf("Access type not available for playback: %s\n", snd_strerror(err));
-        return err;
-    }
+
     /* set the sample format */
     err = snd_pcm_hw_params_set_format(handle, params, format);
-    if (err < 0) {
-        printf("Sample format not available for playback: %s\n", snd_strerror(err));
-        return err;
-    }
+
     /* set the count of channels */
     err = snd_pcm_hw_params_set_channels(handle, params, channels);
-    if (err < 0) {
-        printf("Channels count (%u) not available for playbacks: %s\n", channels, snd_strerror(err));
-        return err;
-    }
+
     /* set the stream rate */
     rrate = rate;
     err = snd_pcm_hw_params_set_rate_near(handle, params, &rrate, 0);
-    if (err < 0) {
-        printf("Rate %uHz not available for playback: %s\n", rate, snd_strerror(err));
-        return err;
-    }
-    if (rrate != rate) {
-        printf("Rate doesn't match (requested %uHz, get %iHz)\n", rate, err);
-        return -EINVAL;
-    }
+
     /* set the buffer time */
     err = snd_pcm_hw_params_set_buffer_time_near(handle, params, &buffer_time, &dir);
-    if (err < 0) {
-        printf("Unable to set buffer time %u for playback: %s\n", buffer_time, snd_strerror(err));
-        return err;
-    }
+
     err = snd_pcm_hw_params_get_buffer_size(params, &size);
-    if (err < 0) {
-        printf("Unable to get buffer size for playback: %s\n", snd_strerror(err));
-        return err;
-    }
+
     buffer_size = size;
     /* set the period time */
     err = snd_pcm_hw_params_set_period_time_near(handle, params, &period_time, &dir);
-    if (err < 0) {
-        printf("Unable to set period time %u for playback: %s\n", period_time, snd_strerror(err));
-        return err;
-    }
+
     err = snd_pcm_hw_params_get_period_size(params, &size, &dir);
-    if (err < 0) {
-        printf("Unable to get period size for playback: %s\n", snd_strerror(err));
-        return err;
-    }
+
     period_size = size;
     /* write the parameters to device */
     err = snd_pcm_hw_params(handle, params);
-    if (err < 0) {
-        printf("Unable to set hw params for playback: %s\n", snd_strerror(err));
-        return err;
-    }
+
     return 0;
 }
 
@@ -167,33 +161,43 @@ static int set_hwparams(snd_pcm_t *handle,
  
 static int write_loop(snd_pcm_t *handle,
 		      signed short *samples,
-		      snd_pcm_channel_area_t *areas, double freq)
+		      snd_pcm_channel_area_t *areas, double freq, int partials)
 {
+    int err;
+    populate_buff(samples, freq, partials);
+    err = snd_pcm_writei(handle, samples, sample_size/2);
+
+  /*
     double phase = 0;
     signed short *ptr;
-    int err, cptr;
+    snd_pcm_sframes_t err;
+    int  cptr;
     int i = 0;
-    while (i++ < 10) {
-      generate_sine(areas, 0, period_size, &phase, freq);
+    double maxruntime = 1;
+    double runtime, prog;
+    time_t start, end;
+    time(&start);
+    time(&end);
+
+    runtime = difftime(end, start);
+    while (runtime < maxruntime) {
+
+        generate_sine(areas, 0, period_size, &phase, freq, partials);
         ptr = samples;
-        cptr = period_size;
+        cptr = period_size; //how many samples write to buffer
+	printf(" period size %d\n", cptr);
         while (cptr > 0) {
-            err = snd_pcm_writei(handle, ptr, cptr);
-            if (err == -EAGAIN)
-                continue;
-            if (err < 0) {
-	      /*
-                if (xrun_recovery(handle, err) < 0) {
-                    printf("Write error: %s\n", snd_strerror(err));
-                    exit(EXIT_FAILURE);
-		    }
-	      */
-                break;  /* skip one period */
-            }
-            ptr += err * channels;
+	    err = snd_pcm_writei(handle, ptr, cptr);
+	    printf("err %ld \n", err);
+	    i++;
+            ptr += err;
             cptr -= err;
         }
+	time(&end);
+	runtime = difftime(end, start);
+	printf("%d\n", i);
     }
+  */
 }
  
 struct transfer_method {
@@ -202,132 +206,65 @@ struct transfer_method {
     int (*transfer_loop)(snd_pcm_t *handle,
                  signed short *samples,
 			 snd_pcm_channel_area_t *areas,
-			 double freq);
+			 double freq, int partials);
 };
  
 static struct transfer_method transfer_methods[] = {
     { "write", SND_PCM_ACCESS_RW_INTERLEAVED, write_loop },
 };
  
-static void help(void)
-{
-    int k;
-    printf(
-"Usage: pcm [OPTION]... [FILE]...\n"
-"-h,--help  help\n"
-"-D,--device    playback device\n"
-"-r,--rate  stream rate in Hz\n"
-"-c,--channels  count of channels in stream\n"
-"-f,--frequency sine wave frequency in Hz\n"
-"-b,--buffer    ring buffer size in us\n"
-"-p,--period    period size in us\n"
-"-m,--method    transfer method\n"
-"-o,--format    sample format\n"
-"-v,--verbose   show the PCM setup parameters\n"
-"-n,--noresample  do not resample\n"
-"-e,--pevent    enable poll event after each period\n"
-"\n");
-        printf("Recognized sample formats are:");
-        for (k = 0; k < SND_PCM_FORMAT_LAST; ++k) {
-                const char *s = snd_pcm_format_name(k);
-                if (s)
-                        printf(" %s", s);
-        }
-        printf("\n");
-        printf("Recognized transfer methods are:");
-        for (k = 0; transfer_methods[k].name; k++)
-            printf(" %s", transfer_methods[k].name);
-    printf("\n");
-}
 
 static int maxFreq = 880;
 static int minFreq = 110;
+static int steps = 6;
 // need to make the translate function depend on the gui buttons (synth modes)
-static void translate_coords(double maxx, double maxy, double x, double y, double *fx)//, int *fy)
+static void translate_coords(double maxx, double maxy, double x, double y, double *fx, int *fy)
 {
   (*fx) = x/maxx * (maxFreq - minFreq) + minFreq;
-  //  (*fy) = y/maxy * steps;
+  (*fy) = y/maxy * steps;
+  printf("y %f maxy %f fy %d\n", y, maxy, *fy);
 }
 
 int make_sound(double maxx, double maxy, double x, double y)
 {
-    struct option long_option[] =
-    {
-        {"help", 0, NULL, 'h'},
-        {"device", 1, NULL, 'D'},
-        {"rate", 1, NULL, 'r'},
-        {"channels", 1, NULL, 'c'},
-        {"frequency", 1, NULL, 'f'},
-        {"buffer", 1, NULL, 'b'},
-        {"period", 1, NULL, 'p'},
-        {"method", 1, NULL, 'm'},
-        {"format", 1, NULL, 'o'},
-        {"verbose", 1, NULL, 'v'},
-        {"noresample", 1, NULL, 'n'},
-        {"pevent", 1, NULL, 'e'},
-        {NULL, 0, NULL, 0},
-    };
     snd_pcm_t *handle;
-    int err; 
     snd_pcm_hw_params_t *hwparams;
-    int method = 0;
     signed short *samples;
-    unsigned int chn;
+    //    unsigned int chn;
     snd_pcm_channel_area_t *areas;
     double freq;
-    // int fy
+    int partials;
 
-    translate_coords(maxx, maxy, x, y, &freq);
+    translate_coords(maxx, maxy, x, y, &freq, &partials);
  
     snd_pcm_hw_params_alloca(&hwparams);
  
-    err = snd_output_stdio_attach(&output, stdout, 0);
-    if (err < 0) {
-        printf("Output failed: %s\n", snd_strerror(err));
-        return 0;
-    }
- 
+    snd_output_stdio_attach(&output, stdout, 0);
+
     printf("Playback device is %s\n", device);
     printf("Stream parameters are %uHz, %s, %u channels\n", rate, snd_pcm_format_name(format), channels);
     printf("Sine wave rate is %.4fHz\n", freq);
-    printf("Using transfer method: %s\n", transfer_methods[method].name);
+    //    printf("Using transfer method: %s\n", transfer_methods[method].name);
  
-    if ((err = snd_pcm_open(&handle, device, SND_PCM_STREAM_PLAYBACK, 0)) < 0) {
-        printf("Playback open error: %s\n", snd_strerror(err));
-        return 0;
-    }
+    snd_pcm_open(&handle, device, SND_PCM_STREAM_PLAYBACK, 0);
+
+    //    printf("%s", transfer_methods)
+    set_hwparams(handle, hwparams, SND_PCM_ACCESS_RW_INTERLEAVED); // transfer_methods[method].access);
     
-    if ((err = set_hwparams(handle, hwparams, transfer_methods[method].access)) < 0) {
-        printf("Setting of hwparams failed: %s\n", snd_strerror(err));
-        exit(EXIT_FAILURE);
-    }
-    
-    if (verbose > 0)
-        snd_pcm_dump(handle, output);
+    //samples = malloc((period_size * channels * snd_pcm_format_physical_width(format)) / 8);
+    samples = malloc(sample_size);
+    //    areas = calloc(channels, sizeof(snd_pcm_channel_area_t));    
+    //    areas[0].addr = samples;
+    //    areas[0].first = 0 * snd_pcm_format_physical_width(format);
+    //    areas[0].step = channels * snd_pcm_format_physical_width(format);
  
-    samples = malloc((period_size * channels * snd_pcm_format_physical_width(format)) / 8);
-    if (samples == NULL) {
-        printf("No enough memory\n");
-        exit(EXIT_FAILURE);
-    }
-    
-    areas = calloc(channels, sizeof(snd_pcm_channel_area_t));
-    if (areas == NULL) {
-        printf("No enough memory\n");
-        exit(EXIT_FAILURE);
-    }
-    for (chn = 0; chn < channels; chn++) {
-        areas[chn].addr = samples;
-        areas[chn].first = chn * snd_pcm_format_physical_width(format);
-        areas[chn].step = channels * snd_pcm_format_physical_width(format);
-    }
+    //    transfer_methods[method].transfer_loop(handle, samples, areas, freq, partials);
+    write_loop(handle, samples, areas, freq, partials);
  
-    err = transfer_methods[method].transfer_loop(handle, samples, areas, freq);
-    if (err < 0)
-        printf("Transfer failed: %s\n", snd_strerror(err));
- 
-    free(areas);
+    //    free(areas);
     free(samples);
+    sleep(1);
+    //might want to open handle when app opens and close it when app closes to avoid the click sound
     snd_pcm_close(handle);
     return 0;
 }
